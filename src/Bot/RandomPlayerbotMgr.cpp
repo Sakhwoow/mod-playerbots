6a86512f0275c7e9cc1a9682849fdacd8a9ef71a
PlayDargoon <VongolaIvanrogue@gmail.com>
2026-09-16 13:05:07 +1100

    Revert "fix(bots): prevent guild bot login/logout thrash from stagger queue"
    This reverts commit f60697cb1c4f3fe39cb2170bb12d9038b47cf54c.

--- src/Bot/RandomPlayerbotMgr.cpp
+++ src/Bot/RandomPlayerbotMgr.cpp
@@ -2940,9 +2940,7 @@ void RandomPlayerbotMgr::ProcessPendingLogouts()
         ObjectGuid botGuid = _pendingGuildBotLogouts.front();
         _pendingGuildBotLogouts.pop_front();
 
-        uint32 botLow = botGuid.GetCounter();
-        // Skip if EnsureGuildBotsOnline re-added this bot while it was queued for logout
-        if (GetPlayerBot(botGuid) && !GetEventValue(botLow, "add"))
+        if (GetPlayerBot(botGuid))
             LogoutPlayerBot(botGuid);
     }
 }
@@ -3119,10 +3117,6 @@ void RandomPlayerbotMgr::EnsureGuildBotsOnline(uint32 guildId, uint32 precompute
 
         SetEventValue(charGuid, "logout", 0, 0);
         SetEventValue(charGuid, "add", 1, sPlayerbotAIConfig.minRandomBotInWorldTime);
-        // Remove from pending logout queue so the stagger doesn't evict a bot we just re-added
-        _pendingGuildBotLogouts.erase(
-            std::remove(_pendingGuildBotLogouts.begin(), _pendingGuildBotLogouts.end(), botGUID),
-            _pendingGuildBotLogouts.end());
         AddPlayerBot(botGUID, 0);
 
         LOG_DEBUG("playerbots", "GuildBotMinOnline: logging in guild bot {} for guild {}", charGuid, guildId);
